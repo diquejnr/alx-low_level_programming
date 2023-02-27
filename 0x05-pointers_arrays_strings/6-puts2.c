dd0ad736906bf4a9191b39cd2f55121309bd938d
Salifu Sadick <sadick.salifu@gmail.com>
2023-02-27 12:58:46 +0000

    hot fox

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,7 +10,7 @@ void puts2(char *str)
 	int longi = 0;
 	int t = 0;
 	char *y = str;
-	int a;
+	int o;
 
 	while (*y != '\0')
 	{
@@ -18,9 +18,9 @@ void puts2(char *str)
 		longi++;
 	}
 	t = longi - 1;
-	for (a = 0; a <= t; a++)
+	for (o = 0; o <= t; o++)
 	{
-		if (a % 2 == 0)
+		if (o % 2 == 0)
 		{
 			_putchar(str[0]);
 		}
