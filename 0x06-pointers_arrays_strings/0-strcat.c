9265c47b8ba654a5dfa99204f068b6b26a761f63
Salifu Sadick <sadick.salifu@gmail.com>
2023-03-02 01:31:41 -0800

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strcat - function that concatenate two strings
+ * @dest: input value
+ * @src: outpu value
+ * Return: dest
+*/
+
+char *_strcat(char *dest, const char *src);
+{
+	char *dest_ptr = dest;
+	const char *src_ptr = src;
+
+	while (*dest_ptr)
+	{
+		dest_ptr++;
+	}
+	while (*src_ptr)
+	{
+		*dest_ptr = *src_ptr;
+		dest_ptr++;
+		src_ptr++;
+	}
+	*dest_ptr = '\0';
+
+	return (dest);
+}
