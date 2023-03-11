bbcf2ee6a45b887dac2889fe79cfba75aa17c67e
Salifu Sadick <sadick.salifu@gmail.com>
2023-03-11 12:01:35 +0000

    3-puts fix

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,8 @@
-#include "main.h"
-void _puts(char *str)
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+void _puts(char *str);
+
 /**
  * main - Entry point
  *
@@ -8,6 +10,11 @@ void _puts(char *str)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	_puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
+
+void _puts(char *str)
+{
+	puts(str);
 }
