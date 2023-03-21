d5a93bdd75ed0b9ce12c7c22b845944951052307
Salifu Sadick <sadick.salifu@gmail.com>
2023-03-21 10:16:46 +0000

    function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib>
+#include "dog.h"
+/**
+ * free_dog - free memory allocated for struct dog
+ * @d: struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
